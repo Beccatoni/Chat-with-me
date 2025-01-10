a58af483d0558e17ba767e537a2824c342023611
Beccatoni <rbccmtni1@gmail.com>
2025-01-10 16:54:11 +0200

    chore: instantiate the io object

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
 })
 
-io.on()
+io.on("connection", (socket)=> {
+    socket.on('send name', (username)=> {
+        io.emit('send name', (username))
+    })
+
+    socket.on('send message', (chat)=> {
+        io.emit('send message', (chat));
+    });
+});
+
+server.listen(port, ()=> {
+    console.log(`Server is listening at the port: ${port}`);
+})
