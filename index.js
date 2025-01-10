4d22da6e0d31de2033f45e0157e3ce3eb77c3cf0
Beccatoni <rbccmtni1@gmail.com>
2025-01-10 16:16:41 +0200

    chore: set up the server and connect

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const {Server} = require('socket.io');
+const http = require('http');
+
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 5000;
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on()
